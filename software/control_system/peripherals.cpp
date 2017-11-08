7497810779a2f632be20296d4870cf8e218a7d66
Gustavo Simas da Silva <gustavosimassilva@gmail.com>
2017-11-08 13:57:15 -0200

    Prototipo 2
    codigo compilado no ATMEGA (dia do teste prototipo 2)

---
+++ software/control_system/peripherals.cpp
@@ -0,0 +1,119 @@
+/*
+ * peripherals.cpp
+ *
+ *  Created on: 25 de ago de 2017
+ *      Author: André
+ */
+
+#include "peripherals.h"
+
+/* Sensor Functions */
+
+void SensorSetup(void) {
+	analogReference(INTERNAL);							// Use 1.1V as analogic reference for the maximum precision of the sensor.
+}
+
+float SensorRoutine(void) {
+	int SensorRead_1, SensorRead_2;
+	float TemperatureC;
+
+	SensorRead_1 = analogRead(SENSOR_PIN_1);			// Read the sensor analogic value.
+	//SensorRead_2 = analogRead(SENSOR_PIN_2);			
+	//TemperatureC = (SensorRead_1 + SensorRead_2) / (SENSOR_CONVERTION * 2);		// Convert the raw temperature to celcius.
+	TemperatureC = SensorRead_1 / SENSOR_CONVERTION;
+
+	return TemperatureC;
+}
+
+/* Display Functions */
+
+// Setup the display comunication pins.
+LiquidCrystal DisplayLCD (DISPLAY_RST, DISPLAY_EN, DISPLAY_D4, DISPLAY_D5, DISPLAY_D6, DISPLAY_D7);
+
+void DisplaySetup(void) {
+	DisplayLCD.begin(DISPLAY_LINE_LENGTH, DISPLAY_NUMBER_LINES);				// The display size, 2 lines and 16 columns.
+}
+
+void DisplayTurnMode(int TurnMode) {
+	switch(TurnMode) {
+		case TURN_ON:
+			DisplayLCD.display();
+			break;
+		case TURN_OFF:
+			DisplayLCD.noDisplay();
+			break;
+	}
+}
+
+void DisplayPrint(const char Header[], float Content, const char Menu[]) {	
+	int TextTab; 
+
+	DisplayLCD.clear();
+
+	TextTab  = (DISPLAY_LINE_LENGTH - strlen(Header)) / 2;						// Calculates necessary tab to center the text.
+
+	DisplayLCD.setCursor(TextTab, 0);											// Selects the First display line and gives a tab to the content.
+	DisplayLCD.print(Header);													// Prints the header.
+	
+	if (Content >= 0) {
+		DisplayLCD.setCursor(TextTab, 1);										// Second line.
+		DisplayLCD.print(Content);												// Print the content information.
+	}
+
+	if (strlen(Menu)) {
+		TextTab  = (DISPLAY_LINE_LENGTH - strlen(Menu)) / 2;					// Menu tab.
+
+		DisplayLCD.setCursor(TextTab, 1);										// Second line.
+		DisplayLCD.print(Menu);													// Print the content information.
+	}
+}
+
+/* Button Functions */
+
+void ButtonSetup(void) {
+	pinMode(BUTTON_PLUS, INPUT);
+	pinMode(BUTTON_MINUS, INPUT);
+	pinMode(BUTTON_NEXT, INPUT);
+	pinMode(SWITCH, INPUT);
+}
+
+int ButtonVerification(int PushedButton) {
+	int ButtonState, ButtonVerification;
+
+	ButtonState = digitalRead(PushedButton);
+
+	if (ButtonState == LOW){ 
+		ButtonVerification = PUSHED;		
+	}
+	else { 
+		ButtonVerification = NOT_PUSHED;
+	}
+
+	return ButtonVerification;
+}
+
+/* Actuator Functions */
+
+void ActuatorSetup(void) {
+	pinMode(ACTUATOR_RELAY, OUTPUT);
+	pinMode(ACTUATOR_INDUCTOR, OUTPUT);
+}
+
+void ActuatorActivation(int TurnMode, int ActuatorSelection) {
+	if(ActuatorSelection == ACTUATOR_RELAY){
+		if (TurnMode) {
+			digitalWrite(ACTUATOR_RELAY, HIGH);	
+		}
+		else {
+			digitalWrite(ACTUATOR_RELAY, LOW);		
+		}
+	}
+	else{
+		if (TurnMode) {
+			digitalWrite(ACTUATOR_INDUCTOR, HIGH);	
+		}
+		else {
+			digitalWrite(ACTUATOR_INDUCTOR, LOW);		
+		}
+	}
+}
---
+++ software/control_system/peripherals.h
@@ -0,0 +1,91 @@
+/*
+ * peripherals.h
+ *
+ *  Created on: 25 de ago de 2017
+ *      Author: André
+ */
+
+#ifndef PERIPHERALS_H_
+#define PERIPHERALS_H_
+
+#include <Arduino.h>
+#include <string.h>
+#include <LiquidCrystal.h>
+#include "hardware_abstraction.h"
+
+// Constants Abstraction
+#define DISPLAY_LINE_LENGTH		16
+#define DISPLAY_NUMBER_LINES	2
+
+#define SENSOR_CONVERTION 		(0.01 / (1.1 / 1024))		// (10 mV per celcius) / (analogic reference) / (ADC range).
+
+#define TURN_ON				1
+#define TURN_OFF			0
+#define NO_CONTENT			-1							// Negative number (Display condition for no content).
+#define NO_MENU				""							// Empty string (Display condition for no menu).							
+#define PUSHED 				1
+#define NOT_PUSHED			0
+
+/*
+ * Function: SensorSetup(void)
+ * For use the temperature sensors, this setup the voltage
+ * reference in 1.1V for the ADC convertion. 
+ */
+void SensorSetup(void);
+
+/*
+ * Function: SensorRoutine(void)
+ * Process the temperature raw data from ADC to celcius.
+ */
+float SensorRoutine(void);
+
+/*
+ * Function: DisplaySetup(void)
+ * Begin the display with 2 lines and 16 columns.
+ */
+void DisplaySetup(void);
+
+/*
+ * Function: DisplayTurnMode(int TurnMode)
+ * Select if the display is turn on or turn off without 
+ * losing the data to be printed.
+ * Parameter: (TurnMode) On or Off.
+ */
+void DisplayTurnMode(int TurnMode);
+
+/*
+ * Function: DisplayPrint(char Header[], float Content, char Menu[])
+ * Print data on the screen.
+ * Parameters: (Header and Menu) Used to pass text data. 
+ * 			   (Content) Pass numeric values.
+ */
+void DisplayPrint(const char Header[], float Content, const char Menu[]);
+
+/*
+ * Function: ButtonSetup(void)
+ * Set button pins as digital inputs.
+ */
+void ButtonSetup(void);
+
+/*
+ * Function: ButtonVerification(int PushedButton)
+ * Read the button or switch state .
+ * Parameter: (PushedButton) Button or switch to verify.
+ * Return: (ButtonVerification) Pushed or non-pushed.
+ */
+int ButtonVerification(int PushedButton);
+
+/*
+ * Function: ActuatorSetup(void)
+ * Set the Actuator pin as digital output.
+ */
+void ActuatorSetup(void);
+
+/*
+ * Function: ActuatorActivation(int TurnMode)
+ * This send a sign HIGH or LOW to the actuator pin for turn on or off.
+ * Parameter: (TurnMode) On or off.
+ */
+void ActuatorActivation(int TurnMode, int ActuatorSelection);
+
+#endif /* PERIPHERALS_H_ */
